ba71c7398ab1e10d2c7feb4e76330bfb93914882
Josip Kotarac <jkotarac@extensionengine.com>
2016-08-19 16:15:04 +0200

    bumped version to reflect changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='1.0.6',
+    version='1.0.7',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
