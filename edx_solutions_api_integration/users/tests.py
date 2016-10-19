8fd2b72b2b592d7649b1a9d893d03501bc80bcd1
Ayesha Baig <ayesha.baig@arbisoft.com>
2016-10-19 12:05:55 +0500

    Fix for story [YONK-415]: code optimized

--- edx_solutions_api_integration/users/tests.py
+++ edx_solutions_api_integration/users/tests.py
@@ -2192,19 +2192,13 @@ class UsersApiTests(SignalDisconnectTestMixin, ModuleStoreTestCase, CacheIsolati
 
     @mock.patch("edx_solutions_api_integration.users.views.module_render.get_module_for_descriptor")
     def test_user_courses_detail_get_undefined_course_module(self, mock_get_module_for_descriptor):
-        course_id = unicode(self.course.id)
-
         # Enroll test user in test course
-        test_uri = '{}/{}/courses'.format(self.users_base_uri, self.user.id)
-        data = {'course_id': course_id}
-
-        response = self.do_post(test_uri, data)
-        self.assertEqual(response.status_code, 201)
+        CourseEnrollmentFactory.create(user=self.user, course_id=self.course.id)
 
         # Get user course details when course_module is None
         mock_get_module_for_descriptor.return_value = None
 
-        test_uri = test_uri + '/' + course_id
+        test_uri = '{}/{}/courses/{}'.format(self.users_base_uri, self.user.id, self.course.id)
         response = self.do_get(test_uri)
 
         self.assertEqual(response.status_code, 200)
--- edx_solutions_api_integration/users/views.py
+++ edx_solutions_api_integration/users/views.py
@@ -970,10 +970,7 @@ class UsersCoursesDetail(SecureAPIView):
             course_descriptor,
             field_data_cache,
             course_key)
-        if course_module is not None:
-            response_data['position'] = course_module.position
-        else:
-            response_data['position'] = None
+        response_data['position'] = getattr(course_module, 'position', None)
         response_data['position_tree'] = {}
         parent_module = course_module
         while parent_module is not None:
