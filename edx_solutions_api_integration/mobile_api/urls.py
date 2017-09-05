ab29234a5647497465b795c0e056f6f66ab88c2a
Aamish Baloch <aamish.baloch@gmail.com>
2017-09-05 16:41:24 +0500

    YONK-750: Oauth support for user course progress API

--- edx_solutions_api_integration/mobile_api/urls.py
+++ edx_solutions_api_integration/mobile_api/urls.py
@@ -8,4 +8,6 @@ from edx_solutions_api_integration.mobile_api import views as mobile_views
 urlpatterns = patterns(
     '',
     url(r'^users/organizations/', mobile_views.MobileUsersOrganizationsList.as_view(), name='mobile-users-orgs-list'),
+    url(r'^users/courses/progress', mobile_views.MobileUsersCourseProgressList.as_view(),
+        name='mobile-users-courses-progress')
 )
--- edx_solutions_api_integration/mobile_api/views.py
+++ edx_solutions_api_integration/mobile_api/views.py
@@ -5,6 +5,7 @@ Views for mobile APIs
 from edx_solutions_api_integration.permissions import MobileListAPIView
 from edx_solutions_api_integration.users.views import (
     UsersOrganizationsList,
+    UsersCourseProgressList,
 )
 from openedx.core.lib.api.permissions import IsStaffOrOwner
 
@@ -12,6 +13,14 @@ from openedx.core.lib.api.permissions import IsStaffOrOwner
 class MobileUsersOrganizationsList(MobileListAPIView, UsersOrganizationsList):
     """
     View to return list of organizations a user belongs to.
+    """
+    def __init__(self):
+        self.permission_classes += (IsStaffOrOwner, )
+
+
+class MobileUsersCourseProgressList(MobileListAPIView, UsersCourseProgressList):
+    """
+    View to return a list of courses user enrolled in and the progress for a user
     """
     def __init__(self):
         self.permission_classes += (IsStaffOrOwner, )
