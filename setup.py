768044b520dfaf2253daf3a607965028bffcb121
J. Cliff Dyer <jcd@sdf.org>
2017-10-05 10:53:30 -0400

    Adjust version to 1.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='1.5.0',
+    version='1.4.2',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
