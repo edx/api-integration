2e8864f580fbc03732213e50fcd92070ce9c5c61
ihtram <m.ihtram@arbisoft.com>
2017-11-09 15:58:45 +0500

    adding documentation for mobileapp notifications apis

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='1.4.5',
+    version='1.5.1',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
