e9e24fa7c3c2428fd253c63ee4c1b779bf4d6151
Aamish Baloch <aamish.baloch@gmail.com>
2018-03-15 19:52:35 +0500

    YONK-927: Key Issue Fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='1.7.2',
+    version='1.7.3',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
