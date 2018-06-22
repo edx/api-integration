616f07820295e15488663250280587e660e4ffcc
Aamish Baloch <aamish.baloch@gmail.com>
2018-06-22 16:56:40 +0500

    YONK-997: Course activeness filter in User Progress API
    Changes include ourse activeness filter added in User Progress API. Filter is optional.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='1.8.3',
+    version='1.8.4',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
