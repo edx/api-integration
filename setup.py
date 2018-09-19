f0de71e2cb97e243aa0f4c0828d7d5fba119e885
Kshitij Sobti <kshitij@sobti.in>
2018-09-19 16:56:57 +0530

    Fix tests for course completion leaders (#147)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.0.0',
+    version='2.0.1',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
