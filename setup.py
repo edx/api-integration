3a3c6c7ba45aef9ef0a35579a9e51c6764674e57
Agrendalath <piotr@surowiec.it>
2018-10-31 14:56:12 +0100

    [MCKIN-8714] Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.2.0',
+    version='2.2.1',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
