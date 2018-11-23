dd0f5f069b3bc2ec200d005c6976d6a7aabe1a84
Agrendalath <piotr@surowiec.it>
2018-11-23 15:51:44 +0100

    Revert "Revert [MCKIN-8730] and [MCKIN-8418] (#167)"
    This reverts commit da62741

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.3.1',
+    version='2.3.0',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
