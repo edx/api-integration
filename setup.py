bc41b468dd150c421e3187aa71e7bcab02128290
Paulo Viadanna <paulo@opencraft.com>
2019-03-14 18:26:26 -0300

    Relay errors in user deletion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.5.12',
+    version='2.5.13',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
