28705c02949ae1d7182af2c19aa76bd0ed566402
Kshitij Sobti <kshitij@sobti.in>
2019-03-20 14:51:45 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.5.13',
+    version='2.6.0',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
