4c95c25d5505d120f70af5cefe615549a51094ea
mudassir-hafeez <mudassir-hafeez@arbisoft.com>
2019-04-15 12:05:43 +0000

    added version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.7.2',
+    version='2.7.4',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
