07de0ff54cb649fbe4823120d2f3fc73c18063f8
Naeem Ilyas <naeem-ilyas@live.com>
2019-05-15 16:43:41 +0000

    review changes

--- edx_solutions_api_integration/tasks.py
+++ edx_solutions_api_integration/tasks.py
@@ -1,15 +1,14 @@
 import logging
-import urllib
 
 from lxml import etree
-from PIL import Image
-from urlparse import urljoin
 from celery.task import task
-
-from django.conf import settings
 
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import CourseKey
+from edx_solutions_api_integration.utils import (
+    replace_static_from_url,
+    get_image_dimensions,
+)
 
 
 logger = logging.getLogger('edx.celery.task')
@@ -68,22 +67,23 @@ def _upgrade_xml_schema(block, course_id, staff_user_id):
     if schema_version > 1:
         return
 
-    logger.info('Updating IE schema for block `{}` in course `{}`'.format(block.parent.block_id, course_id))
-    xmltree.set('schema_version', '2')
-
-    # mark this as updated from script for tracking purpose
-    xmltree.set('script_processed', '1')
     hotspots_element = xmltree.find('hotspots')
     hotspot_elements = hotspots_element.findall('hotspot')
+    update_schema = False
 
     for index, hotspot_element in enumerate(hotspot_elements):
         if not hotspot_element.get('x').endswith('%') or not hotspot_element.get('y').endswith('%'):
+            update_schema = True
             _convert_to_percentage_coordinates(xmltree, hotspot_element, course_id)
 
-    block.data = etree.tostring(xmltree)
-    store.update_item(xblock=block, user_id=staff_user_id)
-    logger.info('Successfully Updated IE schema for block `{}` in course: `{}`'
-                .format(block.parent.block_id, course_id))
+    if update_schema:
+        xmltree.set('schema_version', '2')
+        # mark this as updated from script for tracking purpose
+        xmltree.set('script_processed', '1')
+        block.data = etree.tostring(xmltree)
+        store.update_item(xblock=block, user_id=staff_user_id)
+        logger.info('Successfully Updated IE schema for block `{}` in course: `{}`'
+                    .format(block.parent.block_id, course_id))
 
 
 def _convert_to_percentage_coordinates(xmltree, hotspot, course_id, revert=False):
@@ -92,8 +92,8 @@ def _convert_to_percentage_coordinates(xmltree, hotspot, course_id, revert=False
     height = background.get('height')
 
     if None in (width, height):
-        image_url = _replace_static_from_url(background.get('src'), course_id=course_id)
-        img_size = _get_image_dimensions(image_url)
+        image_url = replace_static_from_url(background.get('src'), course_id=course_id)
+        img_size = get_image_dimensions(image_url)
         if img_size:
             width, height = img_size
 
@@ -104,37 +104,6 @@ def _convert_to_percentage_coordinates(xmltree, hotspot, course_id, revert=False
             width, height = _convert_pixel_to_percentage(width, height, hotspot.get('x'), hotspot.get('y'))
         hotspot.set('x', width)
         hotspot.set('y', height)
-
-
-def _get_image_dimensions(image_url):
-    try:
-        img = Image.open(urllib.urlopen(image_url))
-    except Exception as e:
-        logger.warning('Failed loading image: {}'.format(e.message))
-        return None
-    else:
-        return img.size
-
-
-def _replace_static_from_url(url, course_id):
-    if not url:
-        return url
-    try:
-        from static_replace import replace_static_urls
-    except ImportError:
-        return url
-
-    url = '"{}"'.format(url)
-    lms_relative_url = replace_static_urls(url, course_id=CourseKey.from_string(course_id))
-    lms_relative_url = lms_relative_url.strip('"')
-    return _make_url_absolute(lms_relative_url)
-
-
-def _make_url_absolute(url):
-    lms_base = settings.ENV_TOKENS.get('LMS_BASE')
-    scheme = 'https' if settings.HTTPS == 'on' else 'http'
-    lms_base = '{}://{}'.format(scheme, lms_base)
-    return urljoin(lms_base, url)
 
 
 def _convert_pixel_to_percentage(width, height, x_in_pixel, y_in_pixel):
