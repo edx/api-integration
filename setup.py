996c82680729dd360c4bd2a8110eb42565af086f
wajeeha-khalid <wajeeha.khalid@arbisoft.com>
2019-06-19 23:23:46 +0500

    Revert " [MCKIN-7759] Remove usage of CourseAggregatedMetaData since it is no longer in use. (#222)"
    This reverts commit 56a232dc6df8e73f4383a767ff466736e3cd9cb7.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='2.5.7',
+    version='3.0.0',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
