969017bc4349803e3d12ada4744c804f15780def
Naeem Ilyas <naeem-ilyas@live.com>
2019-09-12 14:19:19 +0000

    MCKIN-11408 - Script for converting Ooyala IDs to Brightcove IDs

--- edx_solutions_api_integration/management/commands/convert_ooyala_ids_to_bcove.py
+++ edx_solutions_api_integration/management/commands/convert_ooyala_ids_to_bcove.py
@@ -9,17 +9,16 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from edx_solutions_api_integration.tasks import update_image_explorer_schema
+from edx_solutions_api_integration.tasks import convert_ooyala_ids_to_bcove
 
 logger = logging.getLogger(__name__)  # pylint: disable=locally-disabled, invalid-name
 
 
 class Command(BaseCommand):
     """
-    Command to update Image Explorer schema to version 2 and convert pixel
-    coordinates to percentages
+    Command to update Ooyala Xblock Content IDs to corresponding Brightcove IDs
     """
-    help = 'Update Image Explorer xblock schema'
+    help = 'Convert Ooyala IDs to corresponding Brightcove IDs'
     batch_size = 100
     option_list = BaseCommand.option_list + (
         make_option(
@@ -30,21 +29,13 @@ class Command(BaseCommand):
         make_option(
             "--course-id",
             dest="course_id",
-            help="Single Course ID to process IE instances in",
-        ),
-        make_option(
-            "--revert",
-            dest="revert",
-            action="store_true",
-            default=False,
-            help="Revert in case something goes wrong for this script"
+            help="Single Course ID to process Ooyala instances in",
         ),
     )
 
     def handle(self, *args, **options):
         course_id = options.get('course_id')
         user_id = options.get('user_id')
-        revert = options.get('revert')
 
         if not user_id:
             raise CommandError("--user-id parameter is missing. Please provide a staff user id")
@@ -55,9 +46,8 @@ class Command(BaseCommand):
                 raise CommandError("Invalid user id: {}. Please provide a valid staff user id".format(user_id))
 
         if course_id:
-            logger.info('IE schema update task queued for Course: {}'.format(course_id))
-            update_image_explorer_schema.delay(user_id, [course_id], revert)
-
+            logger.info('Ooyala IDs update task queued for Course: {}'.format(course_id))
+            convert_ooyala_ids_to_bcove.delay(user_id, [course_id])
         else:
             # run on all open courses
             open_courses = CourseOverview.objects.filter(
@@ -65,10 +55,10 @@ class Command(BaseCommand):
                 Q(end__isnull=True)
             ).values_list('id', flat=True)
 
-            logger.info('IE schema update command: queuing task for {} Open Courses'.format(len(open_courses)))
+            logger.info('Ooyala IDs update command: queuing task for {} Open Courses'.format(len(open_courses)))
 
             for course_ids in self.chunks(open_courses, self.batch_size):
-                update_image_explorer_schema.delay(user_id, course_ids, revert)
+                convert_ooyala_ids_to_bcove.delay(user_id, course_ids)
 
     def chunks(self, l, n):
         """Yield successive n-sized chunks from l."""
