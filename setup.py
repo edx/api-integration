0f65462da689edae60d2b5d0ce91ec5ef80dc3eb
Naeem Ilyas <naeem-ilyas@live.com>
2019-09-23 15:43:09 +0500

    MCKIN-11426 - convert ooyala embeds in raw html and image explorer (#235)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.8',
+    version='3.1.9',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
