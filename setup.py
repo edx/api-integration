ba730118786dbebea549352cba799b58334b04d9
Naeem Ilyas <naeem-ilyas@live.com>
2019-10-16 14:44:18 +0000

    MCKIN-12100 - conversion of ooyala scripts in more block types

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.11',
+    version='3.1.12',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
