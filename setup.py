48bee7ec73e1778a602b098ccaf5a801f1cbf59f
Naeem Ilyas <naeem-ilyas@live.com>
2019-11-19 15:27:15 +0500

    MCKIN-12647 - static tabs added in bcove migration script (#246)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.17',
+    version='3.1.18',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
