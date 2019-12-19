217a0b283816d4e356ff5cded1ba03855c24bdab
Naeem Ilyas <naeem-ilyas@live.com>
2019-12-19 11:24:06 +0000

    MCKIN-12728 - IE embed code update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.20',
+    version='3.1.21',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
