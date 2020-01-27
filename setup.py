d0ac5bfebc083bee438d220ecea999bf65785cd9
Nasir Hussain <nasirhjafri@gmail.com>
2020-01-27 15:18:06 +0500

    MCKIN-12877 Updated version to 3.1.25

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.24',
+    version='3.1.25',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
