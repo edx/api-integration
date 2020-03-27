0b777ac4004b679817f9d0e4a012f5cc0a0fb0c1
Naeem Ilyas <naeem-ilyas@live.com>
2020-03-27 16:46:09 +0000

    VB

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.35',
+    version='3.1.36',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
