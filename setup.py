aa425845f4ed928ab2df29fcfee770cafaaad036
Nasir Hussain <nasirhjafri@gmail.com>
2020-05-04 13:41:14 +0500

    MCKIN-16248 Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.45',
+    version='3.1.46',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
