5b0bf55e816dfd71bbd71f74ca02ca970e8c9380
Nasir Hussain <nasirhjafri@gmail.com>
2020-05-07 17:48:43 +0500

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.48',
+    version='3.1.49',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
