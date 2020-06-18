84ee136e550875a40cd229d522d381521ee14446
ihtram <m.ihtram@arbisoft.com>
2020-06-18 19:09:53 +0500

    update version to 4.0.0 for ironwood

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='3.1.53',
+    version='4.0.0',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
