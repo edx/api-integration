a09e09844045cc7b0cb985ef97e9355acf221e11
root <root@lms.devstack.edx>
2020-07-14 18:20:10 +0000

    version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api-integration',
-    version='4.0.5',
+    version='4.0.6',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
