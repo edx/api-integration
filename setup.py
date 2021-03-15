aa2ccf2e11028d41c886c6b4f03edb7b7a676964
Mudassir Hafeez <mudasir5761@gmail.com>
2021-03-15 20:35:46 +0500

    MCKIN-30999 to master: sync user info with comment service (#329)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='api-integration',
-    version='5.0.2',
+    version='5.0.3',
     description='RESTful api integration for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
